5ea2ead47e9ac375247d3277168a7920dc096633
nehause <wlsgnlwo@gmail.com>
2022-08-05 11:23:23 +0900

    assignmentOperatorExample

---
+++ src/com/jhj/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,31 @@
+package com.jhj.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		
+		// +=
+		
+		int result1 = 0;
+		int result2 = 0;
+		int result3 = 1;
+		
+		result1 += 10;
+		// = result = result + 10;
+		
+		System.out.println("result : " + result1);
+		
+		//-=
+		result2 -= 10;
+		// = result = result - 5;
+		
+		System.out.println("result : " + result2);
+		
+		result3 *= 10;
+		// = result = result * 10;
+		
+		System.out.println("result : " + result3);
+
+	}
+
+}
