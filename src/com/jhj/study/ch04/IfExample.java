f6ded9ae6589f6eaed96be4c9366c4f07166ff4c
Hwijae Jin <wls974@naver.com>
2022-08-09 17:32:51 +0900

    example

--- src/com/jhj/study/ch04/IfExample.java
+++ src/com/jhj/study/ch04/IfExample.java
@@ -3,7 +3,7 @@ package com.jhj.study.ch04;
 public class IfExample {
 
 	public static void main(String[] args) {
-		int score = 92;
+		int score = 62;
 		
 		if(score >= 90) {
 			if(score >= 97) {
@@ -17,13 +17,37 @@ public class IfExample {
 			}
 		}
 		else if(score >= 80 & score <= 89) {
-			System.out.println("B");
+			if(score >= 87) {
+				System.out.println("B+");
+			}
+			else if(score >= 83 & score <= 86) {
+				System.out.println("B");
+			}
+			else {
+				System.out.println("B-");
+			}
 		}
 		else if(score >= 70 & score <= 79) {
-			System.out.println("C");
+			if(score >= 77) {
+				System.out.println("C+");
+			}
+			else if(score >= 73 & score <= 76) {
+				System.out.println("C");
+			}
+			else {
+				System.out.println("C-");
+			}
 		}
-		else if(score >= 60 & score <= 60) {
-			System.out.println("D");
+		else if(score >= 60 & score <= 69) {
+			if(score >= 67) {
+				System.out.println("D+");
+			}
+			else if(score >= 63 & score <= 66) {
+				System.out.println("D");
+			}
+			else {
+				System.out.println("D-");
+			}
 		}
 		else {
 			System.out.println("F");
--- src/com/jhj/study/ch04/IfExample2.java
+++ src/com/jhj/study/ch04/IfExample2.java
@@ -3,7 +3,7 @@ package com.jhj.study.ch04;
 public class IfExample2 {
 
 	public static void main(String[] args) {
-		int grade = 5;
+		int grade = 3;
 		int cost = 10000;
 
 		if(grade == 1) {
@@ -22,9 +22,12 @@ public class IfExample2 {
 			double price = cost*(1-0.2);
 			System.out.println("정가가 " + cost + "원인 골드 등급의 할인가는: " + (int)price + "원 입니다.");
 		}
-		else {
-			double price = cost*(1-0.27);
+		else if(grade == 5) {
+			double price = cost*(1-0.027);
 			System.out.println("정가가 " + cost + "원인 플래티넘 등급의 할인가는: " + (int)price + "원 입니다.");
+		}
+		else {
+			System.out.println("없는 등급입니다. 1부터 5까지의 등급을 지정해주십시오");
 		}
 	}
 
