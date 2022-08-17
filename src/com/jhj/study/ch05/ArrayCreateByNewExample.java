418e69488b00d4dd898d44493ce861a0d6ff8cc0
nehause <wlsgnlwo@gmail.com>
2022-08-17 17:14:20 +0900

    commit

--- src/com/jhj/study/ch05/ArrayCreateByNewExample.java
+++ src/com/jhj/study/ch05/ArrayCreateByNewExample.java
@@ -9,6 +9,17 @@ public class ArrayCreateByNewExample {
 		int[] arr2;
 		arr2 = new int[5];
 		
+		// int[] b = new int[30];
+		
+		arr1[0] = 77;
+		System.out.println("arr1[0] : " + arr1[0]);
+		
+		arr1[1] = 88;
+		System.out.println("arr1[1] : " + arr1[1]);
+		
+		arr1[2] = 99;
+		System.out.println("arr1[2] : " + arr1[2]);
+		
 	}
 
 }
