60e5fd4903ace9725bd90fb308795bbafe88c9f3
nehause <wlsgnlwo@gmail.com>
2022-10-04 14:47:02 +0900

    javaExample

---
+++ src/com/jhj/study/ch15/fifth/PropertiesExample.java
@@ -0,0 +1,36 @@
+package com.jhj.study.ch15.fifth;
+
+import java.io.FileReader;
+import java.net.URLDecoder;
+import java.util.Properties;
+
+public class PropertiesExample {
+
+	public static void main(String[] args) throws Exception {
+		
+		Properties properties  = new Properties();
+		
+		String path = PropertiesExample.class.getResource("database.properties").getPath();
+//		String path = ((object) properties).getResource("database.properties").getPath();
+		path = URLDecoder.decode(path, "utf-8");
+		
+		
+//		FileReader fileReader = new FileReader(path);
+//		properties.load(fileReader);
+
+//		=
+				
+		properties.load(new FileReader(path));
+		
+		String driver = properties.getProperty("driver");
+		String url = properties.getProperty("url");
+		String username = properties.getProperty("username");
+		String password = properties.getProperty("password"); 
+		
+		System.out.println("driver : " + driver);
+		System.out.println("url : " + url);
+		System.out.println("username : " + username);
+		System.out.println("password : " + password);
+	}
+
+}
