a78bd9fd584ead3dfda0b07ed78b5fca78e7a0f5
nehause <wlsgnlwo@gmail.com>
2022-10-06 12:15:50 +0900

    for test

---
+++ src/com/jhj/study/test/test/For.java
@@ -0,0 +1,16 @@
+package com.jhj.study.test.test;
+
+public class For {
+
+	public static void main(String[] args) {
+		for(int i=0; i<3; i++) {
+
+			System.out.println("");
+			for(int j=0; j<3; j++) {
+				System.out.print(j);
+			}
+		}
+
+	}
+
+}
