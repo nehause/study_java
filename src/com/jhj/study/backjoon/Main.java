87ef7c2de1a82ccbb2f462e14e84501f79dc57b8
nehause <wlsgnlwo@gmail.com>
2022-12-02 17:34:04 +0900

    main

---
+++ src/com/jhj/study/backjoon/Main.java
@@ -0,0 +1,30 @@
+package com.jhj.study.backjoon;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		a = sc.nextInt();
+		
+		if(a >= 0 && a <= 100){
+			if(a >= 90 && a <= 100) {
+				System.out.println("A");
+			}
+			else if(a >= 80 && a <= 89){
+				System.out.println("B");
+			} else if(a >= 70 && a <= 79) {
+				System.out.println("C");
+			} else if(a >= 60 && a <= 69) {
+				System.out.println("D");
+			} else {
+				System.out.println("F");
+			}
+		} else {
+			System.out.println("시험 점수 범위를 벗어났습니다.");
+		}
+
+	}
+	
+}
