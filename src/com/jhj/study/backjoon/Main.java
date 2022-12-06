adbddb8595357381345a64d4196e48bbcbf78a5d
nehause <wlsgnlwo@gmail.com>
2022-12-06 16:38:27 +0900

    baekjoon

--- src/com/jhj/study/backjoon/Main.java
+++ src/com/jhj/study/backjoon/Main.java
@@ -5,26 +5,36 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int a;
+		int a, b, c, d, e, f, g;
 		a = sc.nextInt();
+		b = sc.nextInt();
+		c = sc.nextInt();
 		
-		if(a >= 0 && a <= 100){
-			if(a >= 90 && a <= 100) {
-				System.out.println("A");
-			}
-			else if(a >= 80 && a <= 89){
-				System.out.println("B");
-			} else if(a >= 70 && a <= 79) {
-				System.out.println("C");
-			} else if(a >= 60 && a <= 69) {
-				System.out.println("D");
+		
+		if(a >= 1 && a <= 6 && b >= 1 && b <= 6 && c >= 1 && c <= 6) {
+			
+			if(a == b && b == c) {
+				System.out.println(10000 + a*1000);
+			} else if(a==b) {
+				System.out.println(1000 + a*100);
+			} else if(a==c) {
+				System.out.println(1000 + a*100);
+			} else if(b==c) {
+				System.out.println(1000 + b*100);
 			} else {
-				System.out.println("F");
+				if(a > b && a > c) {
+					System.out.println(a*100);
+				} else if(b > a && b > c) {
+					System.out.println(b*100);
+				} else {
+					System.out.println(c*100);
+				}
 			}
+			
 		} else {
-			System.out.println("시험 점수 범위를 벗어났습니다.");
+			System.out.println("주사위의 눈이 맞지 않습니다.");
 		}
-
+		
 	}
 	
 }
